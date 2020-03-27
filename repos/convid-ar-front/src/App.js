d577ec2c5459eef7b1577754cf5c417dc8461b0f
LucaJaich <jaichencol@gmail.com>
2020-03-27 11:26:30 -0300

    foundation feed design added

--- repos/convid-ar-front/src/App.js
+++ repos/convid-ar-front/src/App.js
@@ -1,38 +1,67 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Fundaciones from './components/Collapsible'
+import FoundationFeed from './components/foundationFeed';
+import Navbar from './components/navbar'
+import Donate from './components/donate'
 
 class App extends Component {
 
   constructor(props) {
-    super(props)
-    this.fundaciones = new React.createRef();
+    super(props);
+    this.navbar = React.createRef();
+    this.foundation = React.createRef();
+    this.changeScreenDonate = this.changeScreenDonate.bind(this);
+    this.setScreen = this.setScreen.bind(this);
     this.state = {
-      test: ""
+      screen: "donate"
     }
   }
 
-  callApi = (ip) => {
-    fetch("http://localhost:3100/inicio")
-        .then(res => res.text())
-        .then(res => this.setState({ test: res }));
-    console.log(this.state.test)
+  changeScreenNav = (screen) => {
+    let aux = this.navbar.current.state.colorMenu;
+    this.navbar.current.navNames.map((name) => {
+      switch (name) {
+        case (screen):
+          aux[name] = "#000000";
+          this.setState(state => ({screen: name}))
+          break;
+        default:
+          aux[name] = "#787878";
+          break;
+      }
+    })
+    this.navbar.current.setState(state => ({colorMenu: aux}));
   }
 
-  componentWillMount = () => {
-    this.callApi("inicio");
+  changeScreenDonate = () => {
+    console.log("hola");
+    this.setState(state => ({screen: "donate"}))
   }
+
+  setScreen = () => {
+    switch (this.state.screen) {
+      case "found":
+        return(<FoundationFeed changeScreen={this.changeScreenDonate}/>)
+        break;
+      case "donate":
+        return(<Donate />)
+        break;
+      default:
+        return(<h3>Not page found</h3>)
+    }
+  }
+
   render(props) {
     return (
       <div className="App">
-        <div className="container">
-          <Fundaciones ref={this.fundaciones}/>
-        </div>
+        <Navbar ref={this.navbar} changeScreen={this.changeScreenNav}/>
+        {this.setScreen()}
       </div>
-    )
+    );
   }
-  
 }
 
 export default App;
+
+// TODO: SQUARE AVATAR IMAGE
